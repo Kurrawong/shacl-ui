0e2fa647a98d6e8b5de4b001fdb5c2a12329778e
Edmond Chuc <edmond@kurrawong.ai>
2024-07-13 20:06:41 +1000

    test: fix es module resolution with rdf-ext in jest

--- js/jest.config.js
+++ js/jest.config.js
@@ -2,4 +2,16 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    "^.+\\.ts?$": [
+        "ts-jest",
+      {
+        useESM: true,
+      }
+    ]
+  },
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1"
+  }
 };
